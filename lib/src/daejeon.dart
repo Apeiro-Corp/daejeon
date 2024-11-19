f568e5d77fd010e2d9d59cf945f9459488025959
junsuPark <krpjs0508@gmail.com>
2024-11-19 15:14:03 +0900

    implement daejeon

---
+++ lib/src/daejeon.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+
+/// {@template bloc}
+/// Takes two `Streams` of `Events` and `SideEffects` as input
+/// and transforms them into `Streams` of `States` and `SideEffects` as output.
+/// {@endtemplate}
+abstract class Daejeon<Event, State, SideEffect> extends Bloc<Event, State> {
+  /// {@macro bloc}
+  Daejeon(State initialState)
+      : sideEffect = StreamController(),
+        super(initialState);
+
+  /// Initializes Daejeon with initial state and side effect.
+  Daejeon.from(State initialState, SideEffect initialSideEffect)
+      : sideEffect = StreamController<SideEffect>()..add(initialSideEffect),
+        super(initialState);
+
+  /// Initializes Daejeon with initial state and side effect stream.
+  Daejeon.fromStream({
+    required State initialState,
+    required this.sideEffect,
+  }) : super(initialState);
+
+  final StreamController<SideEffect> sideEffect;
+}
